f0d6d8281561892f886576122d8629149ef9057f
mishohan <mishohan.o786@gmail.com>
2025-08-14 19:53:47 +0800

    Exercise Arrays and Object

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,29 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000) {
     return true;
-  } else {
+  } 
+  else if(number === 10) {
+    return true;
+  }
+    else if ( number === 9) {
     return false;
   }
+  else if( number === 6) {
+    return false;
+  } 
 }
+
+//or just be lazy, if number is greater than 10 its true and if less it's false
+/*
+  function numberChecker(number){
+  if(number >= 10){
+  return ture;
+}
+  else{
+  return false;
+}
+}
+*/
 
 // Do not edit below this line
 module.exports = numberChecker;
