75925dcdaecd8a2769ee18bcfe883ffca7519dd8
mishohan <mishohan.o786@gmail.com>
2025-08-16 00:46:23 +0800

    Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = cleanStr.length - 1;
+    while(left < right){
+        if(cleanStr[left] !== cleanStr[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
