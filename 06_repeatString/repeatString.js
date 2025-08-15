dc947af29ccf26f44004475384ae2f00f8464005
mishohan <mishohan.o786@gmail.com>
2025-08-15 18:49:37 +0800

    function repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    let result = '';
+
+    if(num < 0){
+            result = 'ERROR';
+        }
+
+    else{
+    for(let i = 0; i < num; i++){
+        result += str;
+    }
+        }
+
+    return result;
 };
+
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString("don't use the built-in repeat method!", 1);
+repeatString('goodbye', -1);
+const number = Math.floor(Math.random() * 1000);
+repeatString('hey', number);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
