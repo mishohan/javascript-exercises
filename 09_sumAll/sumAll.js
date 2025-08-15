15ef4a864d6eb89661d85ca0d11a21d2caf78bcb
mishohan <mishohan.o786@gmail.com>
2025-08-15 21:33:34 +0800

    Sum All

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
 
+    if(initial < 0 || final < 0 || !Number.isInteger(initial) || !Number.isInteger(final)) return 'ERROR';
+    if(initial > final){
+         const temp = initial; 
+         initial = final;
+         final = temp;
+         }
+
+    let sum = 0;
+
+    for(let i = initial; i <= final; i++){
+
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
