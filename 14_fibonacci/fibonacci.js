15d060ab4dd7c0a8cc5dbcc4479bb22f2f954d7d
mishohan <mishohan.o786@gmail.com>
2025-08-16 01:17:00 +0800

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    n = Number(n);
+    
+    if(n < 0) return "OOPS";
+    if(n == 0) return 0;
+    if(n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci( n - 2);
 
 };
 
