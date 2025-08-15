db3aebed11360e22155d8626e23637669565cd1c
mishohan <mishohan.o786@gmail.com>
2025-08-15 21:07:55 +0800

    Remove From Array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    return array.filter(item => !numbers.includes(item));
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
