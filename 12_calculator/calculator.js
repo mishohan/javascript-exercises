d360cfafb2a066ba428d3354e18342e3c4feecff
mishohan <mishohan.o786@gmail.com>
2025-08-15 22:39:59 +0800

    Calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) { 
+  return a - b;
+};
+
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum;
+};
+
+const multiply = function(array) {
+
+  if(array.length === 0) return "Empty Array";
+
+  let total = 1;
+  array.forEach(items => total *= items);
+  return total;
+};
+
+const power = function(a, b) {
+  return Math.pow(a, b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0 || n === 1) return 1;
+  else{
+	return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
